fdc0d7e1c49c85b3fe30a0bf4a70880df9972b91
Manjunath S <manjunath12668@gmail.com>
2026-02-16 02:43:02 +0530

    Update CustomerNavbar.jsx

--- src/users/components/Navbar/CustomerNavbar.jsx
+++ src/users/components/Navbar/CustomerNavbar.jsx
@@ -60,7 +60,7 @@ export default function Navbar({ onSearch }) {
 
         <div className="search-wrapper">
           <div className="downarrow">
-            <a href="#footer-site">↓</a>
+            <a href="#footer-site">👇</a>
           </div>
 
           <input
