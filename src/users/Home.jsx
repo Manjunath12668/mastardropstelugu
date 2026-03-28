b5a46cc796fb03b0d31dba544361f5427c013e0f
Manjunath S <manjunath12668@gmail.com>
2026-03-28 19:02:53 +0530

    Update Home.jsx

--- src/users/Home.jsx
+++ src/users/Home.jsx
@@ -14,71 +14,12 @@ export default function Home() {
   const products = useMemo(() => [
     
     {
-      serial_id: 101,
-      product_name: "Boat Airdopes 141 Wireless Earbuds",
-      product_image: "https://m.media-amazon.com/images/I/51HBom8xz7L._SL1500_.jpg",
-      product_link: "https://amzn.to/3Example"
+      serial_id: 01,
+      product_name: "Backflow Smoke Fountain Shivaling",
+      product_image: "https://m.media-amazon.com/images/I/61xd8Xq-clL._SL1500_.jpg",
+      product_link: "https://amzn.to/4lROa9C"
     },
-    {
-      serial_id: 102,
-      product_name: "Noise Smart Watch ColorFit",
-      product_image: "https://m.media-amazon.com/images/I/61epn29QG0L._SL1500_.jpg",
-      product_link: "https://amzn.to/3Example2"
-    },
-    {
-      serial_id: 103,
-      product_name: "Realme Buds 2 Wired Earphones",
-      product_image: "https://m.media-amazon.com/images/I/51HBom8xz7L._SL1500_.jpg",
-      product_link: "https://amzn.to/3Example"
-    },
-    {
-      serial_id: 104,
-      product_name: "Noise Smart Watch ColorFi",
-      product_image: "https://m.media-amazon.com/images/I/61epn29QG0L._SL1500_.jpg",
-      product_link: "https://amzn.to/3Example2"
-    },
-    {
-      serial_id: 105,
-      product_name: "Boat Airdopes Wireless Earbuds",
-      product_image: "https://m.media-amazon.com/images/I/51HBom8xz7L._SL1500_.jpg",
-      product_link: "https://amzn.to/3Example"
-    },
-    {
-      serial_id: 106,
-      product_name: "Noise Smart Watch Color",
-      product_image: "https://m.media-amazon.com/images/I/61epn29QG0L._SL1500_.jpg",
-      product_link: "https://amzn.to/3Example2"
-    },
-    {
-      serial_id: 107,
-      product_name: "Airdopes 141 Wireless Earbuds",
-      product_image: "https://m.media-amazon.com/images/I/51HBom8xz7L._SL1500_.jpg",
-      product_link: "https://amzn.to/3Example"
-    },
-    {
-      serial_id: 108,
-      product_name: "Noise Sm Watch ColorFit",
-      product_image: "https://m.media-amazon.com/images/I/61epn29QG0L._SL1500_.jpg",
-      product_link: "https://amzn.to/3Example2"
-    },
-    {
-      serial_id: 109,
-      product_name: "Boat dopes 141 Wireless Earbuds",
-      product_image: "https://m.media-amazon.com/images/I/51HBom8xz7L._SL1500_.jpg",
-      product_link: "https://amzn.to/3Example"
-    },
-    {
-      serial_id: 110,
-      product_name: "Noise Smart Watch orFit",
-      product_image: "https://m.media-amazon.com/images/I/61epn29QG0L._SL1500_.jpg",
-      product_link: "https://www.karnatakaone.gov.in/PoliceCollectionOfFine/TrafficFineCollection/dUZnOGxNQzFCbEdIckVoQlNaZVV2UT09"
-    },
-    {
-      serial_id: 111,
-      product_name: "Noise Smatch ColorFit",
-      product_image: Product1,
-      product_link: "https://www.karnatakaone.gov.in/PoliceCollectionOfFine/TrafficFineCollection/dUZnOGxNQzFCbEdIckVoQlNaZVV2UT09"
-    }
+    
   ], []);
 
   // 🔥 LIVE SEARCH FILTER
