726efb384505e217353f5f708a7141d3bb53ba8a
Manjunath S <manjunath12668@gmail.com>
2026-03-29 15:39:05 +0530

    Update Home.jsx

--- src/users/Home.jsx
+++ src/users/Home.jsx
@@ -18,18 +18,6 @@ export default function Home() {
       product_name: "Backflow Smoke Fountain Shivaling",
       product_image: "https://m.media-amazon.com/images/I/61xd8Xq-clL._SL1500_.jpg",
       product_link: "https://amzn.to/4lROa9C"
-    },
-    {
-      serial_id: 102,
-      product_name: "Noise Smart Watch ColorFit",
-      product_image: "https://m.media-amazon.com/images/I/61epn29QG0L._SL1500_.jpg",
-      product_link: "https://amzn.to/3Example2"
-    },
-    {
-      serial_id: 103,
-      product_name: "Noise Smart Watch ColorFit",
-      product_image: "https://m.media-amazon.com/images/I/61epn29QG0L._SL1500_.jpg",
-      product_link: "https://amzn.to/3Example2"
     }
     
   ], []);
